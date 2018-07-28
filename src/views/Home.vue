f44f206ac04a88265a40fa78d5a48a71b4d9f549
Josh <joshaaronlevy@gmail.com>
2018-07-28 17:47:57 -0600

    Added necessary components.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
+    <Header/>
     <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Projects/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from "@/components/HelloWorld.vue";
+import Header from "@/components/Header.vue";
+import Projects from "@/components/Projects.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld
+    // HelloWorld,
+    Header,
+    Projects
   }
-}
+};
 </script>
