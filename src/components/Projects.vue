f33379a4a757a7e6ba19d99cd2ae0920449774a4
Josh <joshaaronlevy@gmail.com>
2018-07-28 19:58:29 -0600

    Adding webpack and trying to add custom fonts.

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,15 +1,12 @@
 <template>
   <b-table :items="items" :fields="fields">
-    <template slot="show_details" slot-scope="row">
+    <template slot="languages" slot-scope="row">
       <!-- we use @click.stop here to prevent emitting of a 'row-clicked' event  -->
-      <b-button size="sm" @click.stop="row.toggleDetails" class="mr-2">
-       {{ row.detailsShowing ? 'Hide' : 'Show'}} Details
+      <b-button size="sm" @click.stop="row.toggleDetails" class="mr-2 btn-success">
+       {{ row.detailsShowing ? 'Hide' : 'View'}} Languages
       </b-button>
       <!-- In some circumstances you may need to use @click.native.stop instead -->
       <!-- As `row.showDetails` is one-way, we call the toggleDetails function on @change -->
-      <b-form-checkbox @click.native.stop @change="row.toggleDetails" v-model="row.detailsShowing">
-        Details via check
-      </b-form-checkbox>
     </template>
     <template slot="row-details" slot-scope="row">
       <b-card>
@@ -21,7 +18,6 @@
           <b-col sm="3" class="text-sm-right"><b>Is Active:</b></b-col>
           <b-col>{{ row.item.isActive }}</b-col>
         </b-row>
-        <b-button size="sm" @click="row.toggleDetails">Hide Details</b-button>
       </b-card>
     </template>
   </b-table>
@@ -32,23 +28,28 @@ export default {
   name: "Projects",
   data() {
     return {
-      fields: ["first_name", "last_name", "show_details"],
+      fields: ["project", "languages", "additional_details"],
       items: [
         {
           isActive: true,
           age: 40,
-          first_name: "Dickerson",
-          last_name: "Macdonald"
+          project: "Josh Levy Portfolio"
         },
-        { isActive: false, age: 21, first_name: "Larsen", last_name: "Shaw" },
+        {
+          isActive: false,
+          age: 21,
+          project: "Title One"
+        },
         {
           isActive: false,
           age: 89,
-          first_name: "Geneva",
-          last_name: "Wilson",
-          _showDetails: true
+          project: "Basic Website"
         },
-        { isActive: true, age: 38, first_name: "Jami", last_name: "Carney" }
+        {
+          isActive: true,
+          age: 38,
+          project: "Carney"
+        }
       ]
     };
   }
--- src/main.js
+++ src/main.js
@@ -5,6 +5,20 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import router from './router'
 import store from './store'
+import {
+  library
+} from '@fortawesome/fontawesome-svg-core'
+import {
+  faLessThan,
+  faGreaterThan
+} from '@fortawesome/free-solid-svg-icons'
+import {
+  FontAwesomeIcon
+} from '@fortawesome/vue-fontawesome'
+
+library.add(faLessThan, faGreaterThan)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
