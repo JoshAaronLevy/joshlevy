ec689c2f61dd24e9c4d740f8d1292464e4f647a0
Josh <joshaaronlevy@gmail.com>
2018-08-09 17:44:51 -0600

    Added details for employment history.

--- src/components/EmploymentHistory.vue
+++ src/components/EmploymentHistory.vue
@@ -45,27 +45,31 @@ export default {
           company: "Fireplace Warehouse",
           title: "Marketing/IT/Web Developer",
           employment_date: "August 2017 - March 2018",
-          details: ["Worked a lot", "Never got breaks"]
+          details: [`Redesigned company website. yielding a 300% increase in traffic and 200% increase in lead conversions`, `Managed internal infrastructure hosted on seven Google Cloud servers, supporting 50 employees in three locations`, `Overhauled company eCommerce site, increasing eCommerce revenue by 57%`, `Managed SEM campaigns, increasing traffic by 150% while reducing SEM costs by over 20%`]
         },
         {
-          company: "Self-Employed",
-          title: "Freelance Developer",
-          employment_date: "August 2017 - March 2018"
+          company: "Freelance",
+          title: "Web Developer/Designer",
+          employment_date: "August 2017 - March 2018",
+          details: [`Created three separate client sites in WordPress, adhering to strict deadlines`, `Created and managed over 20 SEM and paid advertising campaigns, yielding a 57% increase in ad revenue for clients`]
         },
         {
           company: "Hillside Software",
           title: "Full-Stack Developer",
-          employment_date: "April 2014 - September 2017"
+          employment_date: "April 2014 - September 2017",
+          details: [`Developed a full-stack responsive web app`, `Utilized AngularJS, Node.js, Parse (MongoDB), Bootstrap, Jade/Pug and Grunt`]
         },
         {
           company: "mGive - Mobile Accord",
           title: "Account Strategist",
-          employment_date: "April 2013 - March 2014"
+          employment_date: "April 2013 - March 2014",
+          details: [`Assisted a wide variety of non-profit organizations with over 50 fundraising and outreach campaigns`, `Managed over 20 fundraising campaigns responsible for raising over $30 million for clients`, `Responsible for creating and presenting financial reports to internal management, as well as clients`]
         },
         {
           company: "Hillside Software",
           title: "Support, Marketing and Training Manager",
-          employment_date: "December 2004 - March 2013"
+          employment_date: "December 2004 - March 2013",
+          details: [`Trained current and prospective clients on using the software in both small and large settings with over 100 presentations delivered`, `Provided assistance to real estate agents on how to continuously implement the company’s products focused on their unique business needs`, `Worked closely with the development team on prioritizing bug fixes and new product development`, `Administered the execution of sales objectives and project progress`]
         }
       ]
     };
