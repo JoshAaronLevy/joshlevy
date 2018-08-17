f1a727489f4f64ae75ec093666dca28f39d1cb46
Josh <joshaaronlevy@gmail.com>
2018-08-17 08:27:28 -0600

    Added new project.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import router from './router'
+import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'vue-awesome/icons'
 import Icon from 'vue-awesome/components/Icon'
 import store from './store'
-import home from './views/Home.vue'
+// import home from './views/Home.vue'
 
 Vue.component('icon', Icon);
 
@@ -15,13 +15,13 @@ Vue.config.productionTip = false
 
 Vue.use(BootstrapVue);
 
-const router = new Vue({
-  mode: 'history',
-  routes: [{
-    path: '/',
-    component: home
-  }]
-})
+// const router = new Vue({
+//   mode: 'history',
+//   routes: [{
+//     path: '/',
+//     component: home
+//   }]
+// })
 
 new Vue({
   router,
