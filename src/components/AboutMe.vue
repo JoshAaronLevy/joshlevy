1936fb68de085b663c25328d8ac0707bc4de59a5
Josh <joshaaronlevy@gmail.com>
2018-08-17 09:31:35 -0600

    Updated photo

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -2,7 +2,7 @@
   <b-container class="bv-example-row">
     <b-row>
       <b-col lg="4">
-        <b-img src="https://s3-us-west-2.amazonaws.com/joshlevyportfolio/Josh-Levy-IDA.png"></b-img>
+        <b-img src="https://s3-us-west-2.amazonaws.com/joshlevyportfolio/josh-headshot.png" alt="josh levy"></b-img>
       </b-col>
       <b-col lg="8"><p>I believe we're living in the most exciting time for technology. Many of the biggest challenges we face now are going to be solved by developers. I am a junior/mid-level full stack web developer with 5 years experience. I have extensive experience with responsive UI/UX design in HTML5, CSS3 and Bootstrap. I also have solid working experience with modern front-end frameworks such as Vue.js and Angular, back-end languages such as Node.js, Express, SQL and NoSQL databases, along with dev ops technologies such as Firebase, Heroku, Docker and AWS. And I wouldn't be able to keep track of it all if it weren't for my strong working knowledge of Git. I take pride in combining my skills to develop apps with the highest standards of security, stability and scalability. But when I'm not geeking out over dev tech, you can catch me geeking out over Hi-Fi audio, or spending a day at my favorite place on earth - a tennis court.</p></b-col>
     </b-row>
