66e3666061bf411dea7abbeee981c142f69520dc
Josh <joshaaronlevy@gmail.com>
2018-08-29 10:55:01 -0600

    Changed popovers in projects to tooltips.

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -11,18 +11,8 @@
         <div class="timeline-label">
           <h2><a href="https://g90-videos.firebaseapp.com/" target="_blank">Dev Videos</a></h2>
           <p>Full CRUD. Find developer videos easily with intuitive filters. Built with Vue.js and MongoDB.</p>
-          <a href="https://g90-videos.firebaseapp.com/" target="_blank"><i id="linkPopover1" class="fas fa-link"></i></a>
-          <b-popover target="linkPopover1"
-              placement="top"
-              triggers="click hover focus"
-              content="View the deployed website in a new tab.">
-          </b-popover>
-          <a href="https://github.com/JoshAaronLevy/gvideos-client" target="_blank"><i id="githubPopover1" class="fab fa-github"></i></a>
-          <b-popover target="githubPopover1"
-              placement="top"
-              triggers="click hover focus"
-              content="View the GitHub repository in a new tab.">
-          </b-popover>
+          <a href="https://g90-videos.firebaseapp.com/" target="_blank"><i v-b-tooltip.hover title="View deployed website in a new tab." class="fas fa-link"></i></a>
+          <a href="https://github.com/JoshAaronLevy/gvideos-client" target="_blank"><i v-b-tooltip.hover title="View GitHub repository in a new tab." class="fab fa-github"></i></a>
           <!-- <i class="fas fa-code"></i> -->
         </div>
       </div>
@@ -36,18 +26,8 @@
         <div class="timeline-label">
           <h2><a href="https://github.com/JoshAaronLevy/portfolio/" target="_blank">Portfolio</a></h2>
           <p>Full CRUD Web Developer portfolio. Built with Vue.js and PostgreSQL.</p>
-          <a href="https://joshlevy.tech/" target="_blank"><i id="linkPopover2" class="fas fa-link"></i></a>
-          <b-popover target="linkPopover2"
-              placement="top"
-              triggers="click hover focus"
-              content="View the deployed website in a new tab.">
-          </b-popover>
-          <a href="https://github.com/JoshAaronLevy/portfolio" target="_blank"><i id="githubPopover2" class="fab fa-github"></i></a>
-          <b-popover target="githubPopover2"
-              placement="top"
-              triggers="click hover focus"
-              content="View the GitHub repository in a new tab.">
-          </b-popover>
+          <a href="https://joshlevy.tech/" target="_blank"><i v-b-tooltip.hover title="View deployed website in a new tab." class="fas fa-link"></i></a>
+          <a href="https://github.com/JoshAaronLevy/portfolio" target="_blank"><i v-b-tooltip.hover title="View GitHub repository in a new tab." class="fab fa-github"></i></a>
           <!-- <i class="fas fa-code"></i> -->
         </div>
       </div>
@@ -61,18 +41,8 @@
         <div class="timeline-label">
           <h2><a href="https://bad-movie-night.firebaseapp.com/" target="_blank">Bad Movie Night</a></h2>
           <p>Find a bad movie to watch. Serve data to end-users with custom API's.</p>
-          <a href="https://bad-movie-night.firebaseapp.com/" target="_blank"><i id="linkPopover3" class="fas fa-link"></i></a>
-          <b-popover target="linkPopover3"
-              placement="top"
-              triggers="click hover focus"
-              content="View the deployed website in a new tab.">
-          </b-popover>
-          <a href="https://github.com/JoshAaronLevy/bad-movie-night" target="_blank"><i id="githubPopover3" class="fab fa-github"></i></a>
-          <b-popover target="githubPopover3"
-              placement="top"
-              triggers="click hover focus"
-              content="View the GitHub repository in a new tab.">
-          </b-popover>
+          <a href="https://bad-movie-night.firebaseapp.com/" target="_blank"><i v-b-tooltip.hover title="View deployed website in a new tab." class="fas fa-link"></i></a>
+          <a href="https://github.com/JoshAaronLevy/bad-movie-night" target="_blank"><i v-b-tooltip.hover title="View GitHub repository in a new tab." class="fab fa-github"></i></a>
           <!-- <i class="fas fa-code"></i> -->
         </div>
       </div>
@@ -86,18 +56,8 @@
         <div class="timeline-label">
           <h2><a href="https://react-dinosaurs-1.herokuapp.com/" target="_blank">React Dinosaur Jobs</a></h2>
           <p>First experience with React. Created job posting site using state management.</p>
-          <a href="https://react-dinosaurs-1.herokuapp.com/" target="_blank"><i id="linkPopover4" class="fas fa-link"></i></a>
-          <b-popover target="linkPopover4"
-              placement="top"
-              triggers="click hover focus"
-              content="View the deployed website in a new tab.">
-          </b-popover>
-          <a href="https://github.com/JoshAaronLevy/react-dinosaurs-1" target="_blank"><i id="githubPopover4" class="fab fa-github"></i></a>
-          <b-popover target="githubPopover4"
-              placement="top"
-              triggers="click hover focus"
-              content="View the GitHub repository in a new tab.">
-          </b-popover>
+          <a href="https://react-dinosaurs-1.herokuapp.com/" target="_blank"><i v-b-tooltip.hover title="View deployed website in a new tab." class="fas fa-link"></i></a>
+          <a href="https://github.com/JoshAaronLevy/react-dinosaurs-1" target="_blank"><i v-b-tooltip.hover title="View GitHub repository in a new tab." class="fab fa-github"></i></a>
           <!-- <i class="fas fa-code"></i> -->
         </div>
       </div>
@@ -111,18 +71,8 @@
         <div class="timeline-label">
           <h2><a href="https://angular-dinosaurs-one.firebaseapp.com/" target="_blank">Angular Dinosaur Jobs</a></h2>
           <p>First experience with Angular 6. Created job posting site using state management.</p>
-          <a href="https://angular-dinosaurs-one.firebaseapp.com/" target="_blank"><i id="linkPopover5" class="fas fa-link"></i></a>
-          <b-popover target="linkPopover5"
-              placement="top"
-              triggers="click hover focus"
-              content="View the deployed website in a new tab.">
-          </b-popover>
-          <a href="https://github.com/JoshAaronLevy/angular-dinosaurs-one" target="_blank"><i id="githubPopover5" class="fab fa-github"></i></a>
-          <b-popover target="githubPopover5"
-              placement="top"
-              triggers="click hover focus"
-              content="View the GitHub repository in a new tab.">
-          </b-popover>
+          <a href="https://angular-dinosaurs-one.firebaseapp.com/" target="_blank"><i v-b-tooltip.hover title="View deployed website in a new tab." class="fas fa-link"></i></a>
+          <a href="https://github.com/JoshAaronLevy/angular-dinosaurs-one" target="_blank"><i v-b-tooltip.hover title="View GitHub repository in a new tab." class="fab fa-github"></i></a>
           <!-- <i class="fas fa-code"></i> -->
         </div>
       </div>
@@ -136,18 +86,8 @@
         <div class="timeline-label">
           <h2><a href="https://lovely-calculator.firebaseapp.com/" target="_blank">JavaScript Calculator</a></h2>
           <p>Interactive Calculator made with JavaScript DOM manipulation.</p>
-          <a href="https://lovely-calculator.firebaseapp.com/" target="_blank"><i id="linkPopover6" class="fas fa-link"></i></a>
-          <b-popover target="linkPopover6"
-              placement="top"
-              triggers="click hover focus"
-              content="View the deployed website in a new tab.">
-          </b-popover>
-          <a href="https://github.com/JoshAaronLevy/levy-calculator" target="_blank"><i id="githubPopover6" class="fab fa-github"></i></a>
-          <b-popover target="githubPopover6"
-              placement="top"
-              triggers="click hover focus"
-              content="View the GitHub repository in a new tab.">
-          </b-popover>
+          <a href="https://lovely-calculator.firebaseapp.com/" target="_blank"><i v-b-tooltip.hover title="View deployed website in a new tab." class="fas fa-link"></i></a>
+          <a href="https://github.com/JoshAaronLevy/levy-calculator" target="_blank"><i v-b-tooltip.hover title="View GitHub repository in a new tab." class="fab fa-github"></i></a>
           <!-- <i class="fas fa-code"></i> -->
         </div>
       </div>
@@ -586,6 +526,10 @@ img {
 }
 
 .popover {
+  font-size: 16px !important;
+}
+
+.popover-body {
   font-size: 16px !important;
 }
 </style>
