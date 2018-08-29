e6c9e0ebd1603b2def7be21395f1d2893f98986a
Josh <joshaaronlevy@gmail.com>
2018-08-29 16:06:38 -0600

    Updated connect with me to icons and fixed alignment.

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -9,10 +9,12 @@
           <p>I believe we're living in the most exciting time for technology. Many of the biggest challenges we face now are going to be solved by developers. I am a junior/mid-level full stack web developer with 5 years of experience.</p>
           <p>My experience includes responsive UI/UX design in HTML5, CSS3 and Bootstrap. I also have solid working experience with modern front-end frameworks such as Vue.js and Angular, and back-end languages such as Node.js, Express, SQL and NoSQL databases. Furthermore, I have worked with dev ops technologies such as Firebase, Heroku, Docker and AWS. But I wouldn't be able to keep track of it all if it weren't for my strong working knowledge of Git.</p>
           <p>I take pride in combining my skills to develop apps with the highest standards of security, stability and scalability. But when I'm not geeking out over dev tech, you can catch me geeking out over Hi-Fi audio, or spending a day at my favorite place on earth - a tennis court.</p>
-          <div>
+          <div class="social">
             <p class="inline">Find me on:</p>
-            <b-button class="social-button github-button" href="https://github.com/JoshAaronLevy" target="_blank">GitHub</b-button>
-            <b-button class="social-button linkedin-button" href="https://linkedin.com/in/JoshAaronLevy" target="_blank">LinkedIn</b-button>
+            <a href="https://linkedin.com/in/JoshAaronLevy" target="_blank"><i class="fab fa-linkedin"></i></a>
+            <a href="https://github.com/JoshAaronLevy" target="_blank"><i class="fab fa-github"></i></a>
+            <!-- <b-button class="social-button github-button" href="https://github.com/JoshAaronLevy" target="_blank">GitHub</b-button>
+            <b-button class="social-button linkedin-button" href="https://linkedin.com/in/JoshAaronLevy" target="_blank">LinkedIn</b-button> -->
           </div>
         </b-col>
       </b-row>
@@ -30,8 +32,10 @@
           <a class="expand-toggle" @click="showCollapse = !showCollapse" aria-controls="collapseAbout" :class="showCollapse ? 'collapsed' : null" :aria-expanded="showCollapse ? 'true' : 'false'" v-b-toggle.collapseAbout>Read {{ showCollapse ? 'Less' : 'More'}}</a>
           <div class="connect-section">
             <p class="inline">Find me on:</p>
-            <b-button class="social-button github-button" href="https://github.com/JoshAaronLevy" target="_blank">GitHub</b-button>
-            <b-button class="social-button linkedin-button" href="https://linkedin.com/in/JoshAaronLevy" target="_blank">LinkedIn</b-button>
+            <a href="https://linkedin.com/in/JoshAaronLevy" target="_blank"><i class="fab fa-linkedin"></i></a>
+            <a href="https://github.com/JoshAaronLevy" target="_blank"><i class="fab fa-github"></i></a>
+            <!-- <b-button class="social-button github-button" href="https://github.com/JoshAaronLevy" target="_blank">GitHub</b-button>
+            <b-button class="social-button linkedin-button" href="https://linkedin.com/in/JoshAaronLevy" target="_blank">LinkedIn</b-button> -->
           </div>
         </b-col>
       </b-row>
@@ -41,13 +45,13 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       showCollapse: false,
       collapseAbout: false
-    }
+    };
   }
-}
+};
 </script>
 
 <style scoped>
@@ -66,9 +70,13 @@ p {
 }
 .connect-section {
   margin-top: 15px;
+  display: flex;
+  align-items: center;
 }
 p.inline {
   font-weight: 700;
+  margin-bottom: 0px !important;
+  padding-right: 7px !important;
 }
 .inline {
   display: inline;
@@ -76,12 +84,21 @@ p.inline {
 .social-button {
   font-weight: 700 !important;
 }
-.linkedin-button {
-  background-color: #0077B5;
+.social {
+  display: inline-flex;
+  align-items: center;
 }
-.github-button {
-  background-color: #222;
-  margin-right: 10px;
+.fa-linkedin {
+  color: #0077b5;
+  margin-right: 7px;
+  margin-left: 7px;
+  font-size: 32px;
+}
+.fa-github {
+  color: #222;
+  margin-right: 7px;
+  margin-left: 7px;
+  font-size: 32px;
 }
 img {
   width: 100%;
